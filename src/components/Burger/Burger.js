622272ff0fb977e7c3285d48771e4fa3a9f666c1
Akshay Palshikar <aksmakesit@gmail.com>
2018-07-30 22:26:57 -0700

    adding ingredients with state of burger bulder (#3)

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,18 @@ import burgerCss from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map(ingredientKey=>{
+        return [...Array(props.ingredients[ingredientKey])]
+                    .map((_,index)=>{
+                       return <BurgerIngredient key={ingredientKey+index} type={ingredientKey}/>;
+                    });
+    });
     return (
         <div className={burgerCss.Burger}>
-            <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='salad' />
-            <BurgerIngredient type='bread-bottom' />
+            {<BurgerIngredient type='bread-top' />}
+                {transformedIngredients}
+            {<BurgerIngredient type='bread-bottom' />}
         </div>
     );
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,17 @@ import Wrapper from './../../hoc/Wrapper';
 import Burger from './../../components/Burger/Burger';
 
 class BurgerBuilder extends Component{
+    state = {
+        ingredients:{
+            salad:3,
+            cheese:2,
+            meat:1
+        }
+    }
     render(){
         return(
                 <Wrapper>
-                    <Burger/>
+                    <Burger ingredients={this.state.ingredients}/>
                     <div>Build Controls</div>
                 </Wrapper>
         );
