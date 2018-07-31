4b892b97808c00152deb3f3adaaf7d9b66e4c8c4
akshaypalshikar <aksmakesit@gmail.com>
2018-07-31 01:01:09 -0700

    build controls working

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import buildControlCss from './BuildControl.css';
 
-const buildControl = (props) =>(
+const buildControl = (props) => (
     <div className={buildControlCss.BuildControl}>
         <div className={buildControlCss.Label}>{props.label}</div>
-        <button className={buildControlCss.Less}>Less</button>
-        <button className={buildControlCss.More}>More</button>
+        <button className={buildControlCss.Less} 
+            onClick={props.remove} 
+            disabled={props.disable}>Less</button>
+        <button className={buildControlCss.More} 
+            onClick={props.add}>More</button>
     </div>
 );
 export default buildControl;
