2ca9390cc99f02ee2f876e93a7a97dd4ef8f08ee
akshaypalshikar <aksmakesit@gmail.com>
2018-07-31 23:40:34 -0700

    price working

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,10 +4,10 @@ import buildControlCss from './BuildControl.css';
 const buildControl = (props) => (
     <div className={buildControlCss.BuildControl}>
         <div className={buildControlCss.Label}>{props.label}</div>
-        <button className={buildControlCss.Less} 
-            onClick={props.remove} 
+        <button className={buildControlCss.Less}
+            onClick={props.remove}
             disabled={props.disable}>Less</button>
-        <button className={buildControlCss.More} 
+        <button className={buildControlCss.More}
             onClick={props.add}>More</button>
     </div>
 );
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,20 +4,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
     let transformedIngredients = Object.keys(props.ingredients)
-    .map(ingredientKey=>{
-        return [...Array(props.ingredients[ingredientKey])]
-                    .map((_,index)=>{
-                       return <BurgerIngredient key={ingredientKey+index} type={ingredientKey}/>;
-                    });
-    }).reduce((arr,element)=>arr.concat(element),[]);
-    console.log('transformedIngredients : ',transformedIngredients);
-    if(transformedIngredients.length===0){
+        .map(ingredientKey => {
+            return [...Array(props.ingredients[ingredientKey])]
+                .map((_, index) => {
+                    return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />;
+                });
+        }).reduce((arr, element) => arr.concat(element), []);
+    console.log('transformedIngredients : ', transformedIngredients);
+    if (transformedIngredients.length === 0) {
         transformedIngredients = <p>Please start adding ingredients.</p>;
     }
     return (
         <div className={burgerCss.Burger}>
             {<BurgerIngredient type='bread-top' />}
-                {transformedIngredients}
+            {transformedIngredients}
             {<BurgerIngredient type='bread-bottom' />}
         </div>
     );
