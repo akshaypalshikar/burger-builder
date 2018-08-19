9ebad2b912214c88d613196cd35e91bd0015f465
akshaypalshikar <aksmakesit@gmail.com>
2018-08-18 22:12:21 -0700

    added navigation and changed structure

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,18 +3,26 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import cssClasses from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Wrapper from '../../../hoc/Wrapper/Wrapper';
 
 const sideDrawer = (props) => {
-    //...
+    let attachedCssClasses = [cssClasses.SideDrawer, cssClasses.Close];
+    if (props.open) {
+        attachedCssClasses = [cssClasses.SideDrawer, cssClasses.Open];
+    }
     return (
-        <div className={cssClasses.SideDrawer}>
-            <div className={cssClasses.Logo}>
-                <Logo />
+        <Wrapper>
+            <Backdrop show={props.open} backdropClicked={props.close} />
+            <div className={attachedCssClasses.join(' ')}>
+                <div className={cssClasses.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Wrapper>
     );
 }
 
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import toolbarCss from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from './../NavigationItems/NavigationItems';
+import DrawerToggle from './DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className={toolbarCss.Toolbar}>
-        <div>MENU</div>
+        <DrawerToggle handleClick={props.sideDrawerToggleHandler} />
         <div className={toolbarCss.Logo}>
             <Logo />
         </div>
