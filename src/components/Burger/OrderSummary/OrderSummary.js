1435ab62f94b5ad67781207c66ac4f704fc817bf
Akshay Palshikar <aksmakesit@gmail.com>
2018-08-19 11:40:49 -0700

    Checkout (#10)
    * backdrop
    * cancel and continue added on model
    * added total price
    * corrected price

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,28 +2,25 @@ import React, { Component } from 'react';
 import Wrapper from '../../../hoc/Wrapper/Wrapper';
 import Button from './../../UI/Button/Button';
 
-class OrderSummary extends Component {
-    //TODO this can be a functional component
-    render() {
-        const ingredientSummary = Object.keys(this.props.ingredients).map(ingredientKey => {
-            return <li key={ingredientKey}>
-                <span style={{ textTransform: 'capitalize' }}>{ingredientKey}:
-                {this.props.ingredients[ingredientKey]}</span></li>;
-        });
-        return (
-            <Wrapper>
-                <h3>Your Order</h3>
-                <p>A delicious burger with the following ingredients:</p>
-                <ul>
-                    {ingredientSummary}
-                </ul>
-                <p><strong>Total Price: {this.props.price}</strong></p>
-                <p>Continue to Checkout...</p>
-                <Button buttonType='Danger' clicked={this.props.buttonCancelClicked} >CANCEL</Button>
-                <Button buttonType='Success' clicked={this.props.buttonContinueClicked} >CONTINUE</Button>
-            </Wrapper>
-        );
-    }
-};
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients).map(ingredientKey => {
+        return <li key={ingredientKey}>
+            <span style={{ textTransform: 'capitalize' }}>{ingredientKey}:
+                {props.ingredients[ingredientKey]}</span></li>;
+    });
+    return (
+        <Wrapper>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p>Continue to Checkout...</p>
+            <Button buttonType='Danger' clicked={props.buttonCancelClicked} >CANCEL</Button>
+            <Button buttonType='Success' clicked={props.buttonContinueClicked} >CONTINUE</Button>
+        </Wrapper>
+    );
+}
 
 export default OrderSummary;
